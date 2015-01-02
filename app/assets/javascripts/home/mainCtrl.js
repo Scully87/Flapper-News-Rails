d136b246114e0a47dc739bc556f4eb4a88bf89b4
Scully87 <scully87.ts@googlemail.com>
2015-01-02 19:33:37 +0000

    modified mainCtrl to save posts to the server

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -1,26 +1,20 @@
-app.controller('MainCtrl', ['$scope','posts',
+angular.module('flapperNews')
+  .controller('MainCtrl', [
+    '$scope',
+    'posts', 
     function($scope, posts) {
-
-        // two way data binding works only on stuff in the $scope
-        $scope.posts = posts.posts;
-
-        $scope.addPost = function() {
-            if (!$scope.title || $scope.title === '') { return; }
-            $scope.posts.push({
-                title: $scope.title,
-                link: $scope.link,
-                upvotes :  0 ,
-                comments: [
-                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-                ]
-            });
-            $scope.title = '';
-            $scope.link = '';
-        }
-
-        $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
-        };
+      $scope.posts = posts.posts
+      $scope.addPost = function() {
+        if($scope.title === ''){ return;}
+        posts.create({
+          title: $scope.title,
+          link: $scope.link, 
+        });
+        $scope.title = '';
+        $scope.link = '';
+      }; 
+      $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+      } 
     }
-]);
+  ])
