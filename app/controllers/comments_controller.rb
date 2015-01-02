d0263f96da2b6963bf475a60be3be7bd499ea15c
Scully87 <scully87.ts@googlemail.com>
2015-01-02 18:17:27 +0000

    added upvote method for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,5 +8,12 @@ class CommentsController < ApplicationController
     comment = post.comments.create(comment_params)
     respond_with post, comment
   end
-  
+
+  def upvote
+    comment = post.comments.find(params[:id])
+    comment.increment!(:upvotes)
+
+    respond_with post, comment
+  end
+
 end
