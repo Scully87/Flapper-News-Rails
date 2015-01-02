a86cbd44dab9ad1b1ca75078b83d00b34c520a9f
Scully87 <scully87.ts@googlemail.com>
2015-01-02 16:49:25 +0000

    updated the templateUrls in app.js

--- app/assets/javascripts/AngularApp.js
+++ app/assets/javascripts/AngularApp.js
@@ -7,12 +7,12 @@ app.config([
         $stateProvider
           .state('home', {
               url: '/home',
-              templateUrl: '/home.html',
+              templateUrl: 'home/_home.html',
               controller: 'MainCtrl'
           })
           .state('posts', {
               url: '/posts/{id}',
-              templateUrl: '/posts.html',
+              templateUrl: 'posts/_posts.html',
               controller: 'PostsCtrl'
           });
 
