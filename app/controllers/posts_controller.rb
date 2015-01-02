5f638aac93eb0738e67652a980909fc0b2f46470
Scully87 <scully87.ts@googlemail.com>
2015-01-02 18:11:47 +0000

    added create method to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,11 @@ class PostsController < ApplicationController
     respond_with Post.all
   end
 
+  def create
+    respond_with Post.create(post_params)
+  end
+
+
 private
 	def post_params
     params.require(:post).permit(:link, :title)
