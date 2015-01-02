5688fe760accef7d1ff4730fdb4ab6e1ae68d22d
Scully87 <scully87.ts@googlemail.com>
2015-01-02 18:16:59 +0000

    added create method for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,10 @@ class CommentsController < ApplicationController
 		def comment_params
 	    params.require(:comment).permit(:body)
 	  end
+
+	def create
+    comment = post.comments.create(comment_params)
+    respond_with post, comment
+  end
+  
 end
