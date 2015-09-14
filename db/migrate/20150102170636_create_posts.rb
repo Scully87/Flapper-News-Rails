8644644e080aadf418f98b2966e216ad9b9af1bf
K-S-A <atillas@ukr.net>
2015-09-14 16:05:40 +0300

    Update 20150102170636_create_posts.rb
    Fix title column name in db.

--- db/migrate/20150102170636_create_posts.rb
+++ db/migrate/20150102170636_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :tite
+      t.string :title
       t.string :link
       t.integer :upvotes
 
